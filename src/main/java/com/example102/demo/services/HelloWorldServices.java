7f539b28e02e2ca3fa5903cdbbf91b984a8b8700
José Yael Varela García <132051688+Yael-Varela@users.noreply.github.com>
2026-03-04 08:41:42 -0600

    cleaning the previous code

--- src/main/java/com/example102/demo/services/HelloWorldServices.java
+++
@@ -1,9 +0,0 @@
-package com.example102.demo.services;
-import org.springframework.stereotype.Service;
-
-@Service
-public class HelloWorldServices {
-    public String generateMessage(String name) {
-        return "Hola " + name;
-    }
-}
