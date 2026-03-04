b21ce59184d2f19e678939a133902c77cd142b5c
José Yael Varela García <132051688+Yael-Varela@users.noreply.github.com>
2026-03-04 09:00:34 -0600

    Pokemon Img, folders and class

---
+++ src/main/java/com/example102/demo/controllers/Pokemon.java
@@ -0,0 +1,10 @@
+package com.example102.demo.controllers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+Public class Pokemon {
+    @Autowired 
+    
+}
---
+++ src/main/java/com/example102/demo/services/PokemonServices.java
@@ -0,0 +1,11 @@
+package com.example102.demo.services;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class PokemonServices{
+    public byte[] getImage(){
+        
+    }
+
+}
